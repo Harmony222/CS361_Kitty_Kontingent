ca67a112100ae260a2413c3a5335acdbca290bca
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2020-11-01 03:27:53 -0800

    add popup InfoWindows to map markers

--- match_me.py
+++ match_me.py
@@ -6,17 +6,20 @@ import json
 
 def filter_trails(trails_list, difficulty):
     trails_list = get_trails(47.60621, -122.3321, 100)
+    # print(trails_list)
     filtered_trails = []
     for trail in trails_list:
+        # filter criteria
         filtered_trails.append((trail['name'], trail['difficulty'], trail['length'],
-                                trail['location'], float(trail['latitude']), float(trail['longitude'])))
+                                trail['location'], float(trail['latitude']), float(trail['longitude']),
+                                trail['summary']))
     return filtered_trails
 
 
 def filtered_trail_locations(filtered_trails):
     location_list = []
     for trail in filtered_trails:
-        location_list.append([str(trail[0]), trail[4], trail[5], 0])
+        location_list.append([str(trail[0]), trail[4], trail[5], trail[6]])
     # print("LOCATION LIST", location_list)
     return json.dumps(location_list)
 
