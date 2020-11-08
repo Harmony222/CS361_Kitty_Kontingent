0ed70e1bae3be8efcb376b327c2115516d2a7d1d
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2020-11-08 15:43:35 -0800

    match me methods for trail difficulty

--- match_me.py
+++ match_me.py
@@ -3,14 +3,68 @@ from trail_list_functions import get_trails
 import requests
 import json
 
+# method 1: difficulties based on fitness level
+fitness_1 = {
+    1: "green",
+    2: "greenBlue",
+    3: "blue"
+}
+fitness_2 = {
+    1: "greenBlue",
+    2: "blue",
+    3: "blueBlack"
+}
+fitness_3 = {
+    1: "blue",
+    2: "blueBlack",
+    3: "black"
+}
 
-def filter_trails(trails_list, difficulty):
-    '''filters trails based on difficulty'''
+# method 2: calculate difficulty (difficulty + fitness level)
+difficulty_num = {
+    2: "green",
+    3: "greenBlue",
+    4: "blue",
+    5: "blueBlack",
+    6: "black",
+}
+
+
+def filter_trails(trails_list, difficulty, fitness):
+    """
+    Filters trails based on difficulty and fitness level.
+    Difficulties are 1, 2, 3 for "easy and chill", "match my fitness level"
+    and "challenge me". Fitness levels are 1, 2, 3.
+    """
+    print("filtered trails called")
     filtered_trails = []
+    # method 1
+    difficulty_dict = {}
+    if fitness == 1:
+        difficulty_dict = fitness_1
+    elif fitness == 2:
+        difficulty_dict = fitness_2
+    elif fitness == 3:
+        difficulty_dict = fitness_3
+
+    # method 2
+    filter_difficulty = difficulty_num[difficulty + fitness]
+
+    # filter trails - trail[3] is eg. "green"
     for trail in trails_list:
-        if difficulty == trail[3]:
+        if filter_difficulty == trail[3]:
             filtered_trails.append(trail)
     return filtered_trails
+
+
+def trail_locations(trails):
+    """List of locations for map trail markers"""
+    locations = []
+    for trail in trails:
+        # name, latitude, longitude, summary
+        locations.append([trail[1], trail[10], trail[11], trail[12]])
+
+    return json.dumps(locations)
 
 
 def filtered_trail_locations(filtered_trails):
