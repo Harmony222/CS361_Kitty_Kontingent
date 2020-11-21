20a580ffc05c632bf1201ed2839a5069566bd840
lfbettcher <57979763+lfbettcher@users.noreply.github.com>
2020-11-20 19:50:44 -0800

    change filter calculation, set table and map height relative to window height

--- match_me.py
+++ match_me.py
@@ -3,52 +3,25 @@ from trail_list_functions import get_trails
 import requests
 import json
 
-# method 1: difficulties based on fitness level
-fitness_1 = {
-    1: "green",
-    2: "greenBlue",
-    3: "blue"
-}
-fitness_2 = {
-    1: "greenBlue",
-    2: "blue",
-    3: "blueBlack"
-}
-fitness_3 = {
-    1: "blue",
-    2: "blueBlack",
-    3: "black"
-}
-
-# method 2: calculate difficulty (difficulty + fitness level)
+# desired difficulty = feeling selection + fitness level
 difficulty_num = {
     2: "green",
     3: "greenBlue",
     4: "blue",
     5: "blueBlack",
     6: "black",
+    7: "dblack"
 }
 
 
-def filter_trails(trails_list, difficulty, fitness):
+def filter_trails(trails_list, feeling, fitness):
     """
-    Filters trails based on difficulty and fitness level.
-    Difficulties are 1, 2, 3 for "easy and chill", "match my fitness level"
-    and "challenge me". Fitness levels are 1, 2, 3.
+    Filters trails based on feeling selection and fitness level.
+    Feelings are 1, 2, and 3 for "easy and chill", "match my fitness level"
+    and "challenge me!". Fitness levels are 1-4.
     """
-    print("filtered trails called")
     filtered_trails = []
-    # method 1
-    difficulty_dict = {}
-    if fitness == 1:
-        difficulty_dict = fitness_1
-    elif fitness == 2:
-        difficulty_dict = fitness_2
-    elif fitness == 3:
-        difficulty_dict = fitness_3
-
-    # method 2
-    filter_difficulty = difficulty_num[int(difficulty) + int(fitness)]
+    filter_difficulty = difficulty_num[int(feeling) + int(fitness)]
 
     # filter trails - trail[3] is eg. "green"
     for trail in trails_list:
@@ -58,21 +31,18 @@ def filter_trails(trails_list, difficulty, fitness):
 
 
 def trail_locations(trails):
-    """List of locations for map trail markers"""
+    """
+    Returns a list of trails data for map trail markers.
+    Data is formatted for the map javascript code.
+    """
     locations = []
     for trail in trails:
-        # 0:name, 1:latitude, 2:longitude, 3:summary, 4:length, 5:rating, 6: difficulty, 7:location, 8:directions_url, 9:gear_url
-        locations.append([trail[1], trail[10], trail[11], trail[12], trail[2], trail[4], trail[3], trail[5], trail[13], trail[14]])
+        # 0:name, 1:latitude, 2:longitude, 3:summary, 4:length,
+        # 5:rating, 6: difficulty, 7:location, 8:directions_url, 9:gear_url
+        locations.append([trail[1], trail[10], trail[11], trail[12], trail[2],
+                          trail[4], trail[3], trail[5], trail[13], trail[14]])
 
     return json.dumps(locations)
-
-
-def filtered_trail_locations(filtered_trails):
-    location_list = []
-    for trail in filtered_trails:
-        location_list.append([str(trail[0]), trail[4], trail[5], trail[6]])
-    # print("LOCATION LIST", location_list)
-    return json.dumps(location_list)
 
 
 def get_map_api_key():
