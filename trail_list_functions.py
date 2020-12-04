3cbbfad3f61419e0f14480b17e385ec4b2f182ea
Zack <zackismet@gmail.com>
2020-12-03 20:49:11 -0800

    Fixed issue #15

--- trail_list_functions.py
+++ trail_list_functions.py
@@ -14,9 +14,10 @@ def get_trails(lat, lon, dist):
     base_url = "https://www.hikingproject.com/data/get-trails?lat=" + str(lat) \
                 + "&lon=" + str(lon) + "&maxDistance=" + str(dist) + "&key=" + trails_api_key + "&maxResults=100"
     
-    # dict of trail dicts
-    trails = requests.get(base_url).json()['trails']
-    trails_list = []
+    # dict of trail dicts and empty list for returned trails
+    trails, trails_list = requests.get(base_url).json()['trails'], []
+
+    # get desired trail info from dicts and create trails list
     for trail in trails:
         # "as the crow flies" distance - road distance would need an API call for every trail
         distance = get_straight_distance(lat, lon, trail['latitude'], trail['longitude'])
@@ -27,13 +28,16 @@ def get_trails(lat, lon, dist):
         # to navigate to gear page
         gear_url = "/gear?trail_id=" + str(trail["id"])
 
-        # Checks if trail summary needs to be changed  
-        summary = get_trail_summary(trail['summary'])
+        # Checks if trail summary or difficulty need to be changed
+        missing = ['', 'Needs Summary', 'missing']
+        if trail['summary'] in missing: trail['summary'] = '\n'
+        if trail['difficulty'] in missing: trail['difficulty'] = 'dblack'
 
         # sets with trail info appended to list
         trails_list.append((trail["id"], trail['name'], trail['length'], trail['difficulty'], trail['stars'],
                             trail['location'], trail['url'], trail['imgMedium'], trail['high'], trail['low'],
-                            trail['latitude'], trail['longitude'], summary, directions_url, gear_url, distance))
+                            trail['latitude'], trail['longitude'], trail['summary'], 
+                            directions_url, gear_url, distance))
     
     return trails_list
 
@@ -72,37 +76,14 @@ def set_custom_difficulty(difficulty):
     
     return custom_difficulty
 
-def get_trail_summary(old_summary):
-    '''Returns new trail summary if change needed. Otherwise, return old_summary'''
-
-    new_summary = old_summary
-
-    # If old summary is "Needs summary" or is empty, change to newline
-    if old_summary == 'Needs Summary' or old_summary == '':
-        new_summary = '\n'
-
-    return new_summary
-
 def get_straight_distance(lat1, lon1, lat2, lon2):
     '''calculates straight distance between two points in miles'''
-
-    # # radius of earth in miles
-    # rad = 3958.8
 
     # # make sure all variables are floats for conversion, then convert
     lat1, lon1, lat2, lon2 = map(float, [lat1, lon1, lat2, lon2])
 
-    # # convert to radians and get differences
-    # dlat = radians(lat2 - lat1)
-    # dlon = radians(lon2 - lon1)
-
-    # # math for distance percent accross surface of a sphere
-    # a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-
-    # # convert to distance accross sphere surface and then miles accross earth
-    # dist = round(2 * 3958.8 * asin(sqrt(a)), 2)
-
     # math is too hard, use libraries
+    # converts haversine to miles
     loc1, loc2 = (lat1, lon1), (lat2, lon2)
     dist = round(hs.haversine(loc1, loc2) * 0.621371192, 2)
 
